f0de8f57f38d31fb40af540541a1365b78e4b008
claudiutamas <claudiutamas79@gmail.com>
2022-01-22 05:06:25 +0200

    Save cover in DB

--- models/book.js
+++ models/book.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const path = require('path')
-
-const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
   title: {
@@ -19,12 +16,16 @@ const bookSchema = new mongoose.Schema({
     type: Number,
     required: true
   },
-  createAt: {
+  createdAt: {
     type: Date,
     required: true,
     default: Date.now
   },
-  coverImageName: {
+  coverImage: {
+    type: Buffer, // buffer of upload image
+    required: true
+  },
+  coverImageType: {
     type: String,
     required: true
   },
@@ -38,11 +39,11 @@ const bookSchema = new mongoose.Schema({
 // Creates a new property 'coverImagePath' from the existing ones
 bookSchema.virtual('coverImagePath').get(function () {
   // Use function() to have acces at 'this'
-  if (this.coverImageName != null) {
-    return path.join('/', coverImageBasePath, this.coverImageName)
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${
+      this.coverImageType
+    };charset=utf-8;base64,${this.coverImage.toString('base64')}`
   }
 })
 
 module.exports = mongoose.model('Book', bookSchema)
-// export like variable for Book
-module.exports.coverImageBasePath = coverImageBasePath
